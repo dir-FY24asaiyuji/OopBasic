9aa00db35fa026eeced747c6c1f314578408286a
YujiAsai(浅井有志) <yuji.asai.a845@dirbato.co.jp>
2024-04-09 10:31:55 +0900

    4/9

---
+++ OopBasic/src/company/s.java
@@ -0,0 +1,15 @@
+package company;
+
+public class s {
+
+    public static void main(String[] args) {
+        String str = "Hello World!";
+       
+        System.out.println(str.substring(0
+                , 4
+                
+                )); 
+
+    }
+
+}
