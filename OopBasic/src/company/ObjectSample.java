9e12beb147c10eb5914c70c8353e02f4ad330c48
YujiAsai(浅井有志) <yuji.asai.a845@dirbato.co.jp>
2024-04-09 12:52:53 +0900

    4/9

--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -3,12 +3,13 @@ package company;
 public class ObjectSample {
 
     public static void main(String[] args) {
-        var department =new Department("営業部", "XX", １００００００);
-        var employee =new Employee("鈴木", department, "課長", １００);
- 
-        employee.report();
-        employee.report(2);
-        employee.joinMeeting();
+
+        var salesDepartment = new Department("営業部", "xx", 1000000);
+        Employee sales = new Sales("鈴木", salesDepartment, "課長", 100);
+
+        sales.report();
+        sales.report(2);
+        sales.joinMeeting();
         
         System.out.println("");
         var devDepartment = new Department("開発部", "yy", 0);
@@ -16,6 +17,25 @@ public class ObjectSample {
         engineer.report();
         engineer.joinMeeting();
         engineer.developSoftware();
+        
+        System.out.println("");
+        Employee projectManager = new Engineer("佐藤", devDepartment, "PM", 99, "Java");
+    
+        projectManager.report();
+        projectManager.joinMeeting();
+        if (projectManager instanceof Engineer) {
+          
+            ((Engineer) projectManager).developSoftware();
+        }
+        
+        System.out.println("");
+
+        var parttimeWorker = new ParttimeWorker("太田", salesDepartment);
+
+        ((Workable) sales).work();
+        ((Workable) engineer).work();
+        ((Workable) projectManager).work();
+        ((Workable) parttimeWorker).work();
     }
 
 }
